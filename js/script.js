5900cdfcc8980391dba443b8e76aeec012e31768
kauederp <36017642+kauederp@users.noreply.github.com>
2022-06-17 16:46:46 -0300

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,14 @@
+// Script auxiliar
+
+var removeRaio = (id)=>{
+    document.getElementById(id).remove()
+}
+
+
+var date = new Date()
+document.getElementsByClassName('date')[0].innerText = date.getFullYear()
+
+
+
+
+
